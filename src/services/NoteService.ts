99a7276cc203ebf8030b21d98079170f9fd85f17
BrendaSaulit <brenda.saulit@gmail.com>
2025-04-20 16:16:13 -0300

    feat: add delete method to NoteService

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -4,4 +4,14 @@ export class NoteService {
   static async create(data: { note: string; task_id: number }) {
     return await NoteRepository.create(data);
   }
+
+   static async delete(id: number) {
+      const note = await NoteRepository.findById(id);
+  
+      if (!note) {
+        throw new Error('Note not found');
+      }
+  
+      return await NoteRepository.delete(id);
+    }
 }
