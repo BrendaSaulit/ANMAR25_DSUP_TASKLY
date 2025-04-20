22933849fdba3c873602f1acebbda98734d93fee
BrendaSaulit <brenda.saulit@gmail.com>
2025-04-20 15:17:05 -0300

    feat: add delete method to TaskController

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -6,4 +6,17 @@ export class TaskController {
     const task = await TaskService.create(req.body);
     res.status(201).json(task);
   }
+
+
+  static async delete(req: Request, res: Response) {
+    const id = Number(req.params.id);
+
+    try {
+      await TaskService.delete(id);
+      res.status(204).send(); // 204 = No Content
+    } catch (error: any) {
+      res.status(404).json({ error: error.message });
+    }
+  }
+
 }
