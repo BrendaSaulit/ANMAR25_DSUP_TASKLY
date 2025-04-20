221b8e8aeafd15859c5c0b6da7db476fdd94b7f2
BrendaSaulit <brenda.saulit@gmail.com>
2025-04-20 12:16:49 -0300

    feat: add create method to NoteRepository

---
+++ src/repositories/NoteRepository.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
+
+export class NoteRepository {   
+  static async create(data: { note: string; task_id: number;}) {
+    return await prisma.note.create({ data });
+  }
+}
