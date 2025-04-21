450cfe705821776b18028088ac161ef9949006d7
BrendaSaulit <brenda.saulit@gmail.com>
2025-04-21 18:56:26 -0300

    feat: add validation to Task and Note update routes

--- src/routes/noteRoute.ts
+++ src/routes/noteRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { ValidationMiddleware } from "../middlewares/ValidationMiddleware";
 import { NoteController } from "../controllers/NoteController";
 
 const router = Router();
 
-router.post("/tasks/:taskId/notes", NoteController.create);
+router.post("/tasks/:taskId/notes", ValidationMiddleware.validateNote, NoteController.create);
 router.delete("/notes/:id", NoteController.delete);
 router.get("/notes/:id", NoteController.getById);
 router.get("/tasks/:taskId/notes", NoteController.getAll);
-router.put("/notes/:id", NoteController.update);
+router.put("/notes/:id", ValidationMiddleware.validateNote, NoteController.update);
 
 export default router;
