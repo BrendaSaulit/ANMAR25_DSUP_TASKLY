3e0e8d816f362db1b377fb5fbc44ac99f77f7ed2
BrendaSaulit <brenda.saulit@gmail.com>
2025-04-21 12:11:29 -0300

    feat: add update route for Note

--- src/routes/noteRoute.ts
+++ src/routes/noteRoute.ts
@@ -7,5 +7,6 @@ router.post('/tasks/:taskId/notes', NoteController.create);
 router.delete('/notes/:id', NoteController.delete)
 router.get('/notes/:id', NoteController.getById)
 router.get('/tasks/:taskId/notes', NoteController.getAll)
+router.put('/notes/:id', NoteController.update)
 
 export default router;
